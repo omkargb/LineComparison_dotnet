0b8c729e5f18f151fe07d72df88a3985e4afdda5
omkargb <omkargb107@gmail.com>
2021-07-29 19:08:05 +0530

    [omkargb] Refactor : Updated for assignment8 - Calculate Length using Equals and CompareTo

--- LineComparison/CalcLength.cs
+++ LineComparison/CalcLength.cs
@@ -7,16 +7,7 @@ namespace LineComparison
     public class CalcLength
     {
 
-        public int x1, x2, y1, y2;
-        public CalcLength(int x1, int y1, int x2, int y2)
-        {
-            this.x1 = x1;
-            this.x2 = x2;
-            this.y1 = y1;
-            this.y2 = y2;
-        }
-
-        public double ShowResult()
+        public double CalcResult(int x1, int y1, int x2, int y2)
         {
             int point1 = (x2 - x1);
             int point2 = (y2 - y1);
@@ -25,6 +16,22 @@ namespace LineComparison
             Console.WriteLine(" Length of line : " + result + " units\n");
             return result;
         }
-  
+
+        public void displayResult(double length1, double length2)
+        {
+            int eqCheck = (length1.CompareTo(length2));
+            if (eqCheck.Equals(0))
+            {
+                Console.WriteLine(" Result : Both the lines are having same length or both lines are equal.");
+            }
+            else if (eqCheck.Equals(-1))
+            {
+                Console.WriteLine(" Result : Length of FirstLine Is lesser Than secondLine");
+            }
+            else
+            {
+                Console.WriteLine(" Result : Length of FirstLine Is Greater Than secondLine");
+            }
+        }
     }
 }
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,24 +7,13 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Line Comparison Computation Program \n");
-            CalcLength cal1 = new CalcLength(12, 2, 4, 8);
-            CalcLength cal2 = new CalcLength(3, 2, 8, 2);
-            double length1 = cal1.ShowResult();
-            double length2 = cal2.ShowResult();
+            CalcLength cal1 = new CalcLength();
+            CalcLength cal2 = new CalcLength();
+            double length1 = cal1.CalcResult(3, 2, 9, 2);
+            double length2 = cal2.CalcResult(12, 2, 4, 8);
 
-            int eqCheck = (length1.CompareTo(length2));
-            if (eqCheck == 0)
-            {
-                Console.WriteLine(" Result : Both the lines are having same length or both lines are equal.");
-            }
-            else if (eqCheck == 1)
-            {
-                Console.WriteLine(" Result : Length of FirstLine Is Greater Than secondLine");
-            }
-            else
-            {
-                Console.WriteLine(" Result : Length of FirstLine Is lesser Than secondLine");
-            }
+            CalcLength cal3 = new CalcLength();
+            cal3.displayResult(length1, length2);
         }
     }
 }
